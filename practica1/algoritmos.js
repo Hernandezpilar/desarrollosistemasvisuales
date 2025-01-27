dffb574b86ee6a2a78a032d6dc1578b371154b45
Pilar Hernandez <cuh51239922@cuh.mx>
2025-01-27 09:27:55 -0600

    Add files via upload

---
+++ practica1/algoritmos.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('btn');
+        
+        
+        btn.addEventListener('click', () => {
+            alert('Has clickado el botón Pilar Eutimia Hernandez Ramirez');
+            console.log('Pilar Eutimia Hernandez Ramirez ha dado click en el botón este es mi archivo .html y mi archivo .js');
+        });
